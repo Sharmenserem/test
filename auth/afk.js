6609fc9dc759d72ccf2750966cf5b77534fb5cbd
Sharmenserem <shammelody43@gmail.com>
2024-08-01 14:04:06 +0300

    Add files via upload

--- auth/afk.js
+++ auth/afk.js
@@ -1,94 +1,53 @@
-// Importez dotenv et chargez les variables d'environnement depuis le fichier .env
-require("dotenv").config();
+const {zokou} = require("../framework/zokou") ;
+const afkfunc = require("../bdd/afk") ;
 
-const { Pool } = require("pg");
 
-// Utilisez le module 'set' pour obtenir la valeur de DATABASE_URL depuis vos configurations
-const s = require("../set");
+zokou({
+    nomCom : 'afk',
+    categorie : 'Mods',
+  } , async (dest,zk,commandeOptions) => {
+  
+   const {ms , repondre ,superUser , arg} = commandeOptions ;
+  
+   if (!superUser) {repondre('you are not allowed to use this command') ; return}
+  
+   if (!arg[0]) {
 
-// Récupérez l'URL de la base de données de la variable s.DATABASE_URL
-var dbUrl=s.DATABASE_URL?s.DATABASE_URL:"postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9"
-const proConfig = {
-  connectionString: dbUrl,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-};
+        let result = await afkfunc.changeAfkState(1,"on") ;
 
-// Créez une pool de connexions PostgreSQL
-const pool = new Pool(proConfig);
+        if (result === "not defined") {
+            
+            repondre("You have not updated a parameter for AFk (Away From Keyboard)\n To record an afk message please enter a message after the command then an image link (the link is optional)") ;
+        
+        } else {
 
-const creerTableAfk = async () => {
-    try {
-      await pool.query(`
-        CREATE TABLE IF NOT EXISTS afk (
-          id serial PRIMARY KEY,
-          etat text DEFAULT 'off',
-          message text,
-          lien text
-        );
-      `);
-      console.log("La table 'afk' a été créée avec succès.");
-    } catch (e) {
-      console.error("Une erreur est survenue lors de la création de la table 'afk':", e);
+            await afkfunc.changeAfkState(1,"on") ;
+            repondre("The AFK message has been activated") ;
+        }
+    } else {
+
+        try {
+
+        let text = [] ;
+        let url = "no url";
+
+        arg.forEach(element => {
+            
+            if (element.endsWith(".jpg") || element.endsWith(".png") || element.endsWith(".jpeg")){
+                url = element ;
+            } else if (element != undefined) {
+                text.push(element) ;
+            }
+        });
+
+        await afkfunc.addOrUpdateAfk(1,text.join(" "),url) ;
+
+        repondre("The afk has been saved, retype the afk command to activate it") ;
+
+        } catch (error) {
+            console.log(error) ;
+            repondre("An error occurred while saving the afk") ;
+        }
     }
-  };
-
-  creerTableAfk() ;
-
-async function addOrUpdateAfk(id, message, lien) {
-    try {
-       await pool.query(`
-        INSERT INTO afk (id, message, lien)
-        VALUES ($1, $2, $3)
-        ON CONFLICT (id)
-        DO UPDATE SET message = $2, lien = $3;
-      `, [id, message, lien]);
-
-      console.log("L'enregistrement AFK a été ajouté ou mis à jour avec succès.");
-    } catch (e) {
-      console.error("Une erreur est survenue lors de l'ajout ou de la mise à jour de l'enregistrement AFK:", e);
-    }
-}
-
-async function getAfkById(id) {
-    try {
-    const {rows} = await pool.query(`
-        SELECT * FROM afk
-        WHERE id = $1;
-      `, [id]);
-
-      return rows[0];
-    } catch (e) {
-      console.error("Une erreur est survenue lors de la récupération de l'enregistrement AFK par ID:", e);
-      return null;
-    }
-} ;
-
-async function changeAfkState(id, etat) {
-    try {
-      const result = await pool.query(`
-        UPDATE afk
-        SET etat = $1
-        WHERE id = $2
-        RETURNING *;
-      `, [etat, id]);
-
-      if (result.rows.length === 0) {
-          console.log("L'enregistrement AFK n'existe pas.");
-          return "not defined";
-      } else {
-          console.log("L'état de l'enregistrement AFK a été modifié avec succès.");
-          return "succes" ;
-      }
-    } catch (e) {
-      console.error("Une erreur est survenue lors du changement de l'état de l'enregistrement AFK:", e);
-    }
-}
-
-module.exports = {
-
-      addOrUpdateAfk,
-      getAfkById,
-      changeAfkState
-}
+  
+  }) ;
